d79c3f1fc87a60da895e6196d796391b38138803
Chandra Yang <otama.ariq@gmail.com>
2017-07-11 04:14:43 +0700

    simple test for verified digest result

--- md5.rkt
+++ md5.rkt
@@ -95,3 +95,17 @@
   (word-limit (+ w0 w1)))
 
 (define (split message)(bytes-split message 64))
+
+;Unit test
+(module+ test ;; add submodule test
+  (require rackunit)
+  (check-equal? (digest #"Runs all of the tests specified by check forms in the current module and reports the") "21c0b2cbeee1c110d97c32925a469eeb")
+  (check-not-equal? (digest #"Runs all of the tests specified by check forms in the current module and reports the ") "21c0b2cbeee1c110d97c32925a469eeb")
+
+  ;Test
+  (require test-engine/racket-tests)
+  (check-expect (digest #"a") "0cc175b9c0f1b6a831c399e269772661")
+  (check-expect (digest #"Runs all of the tests specified by check forms in the current module and reports the") "21c0b2cbeee1c110d97c32925a469eeb")
+  ;;run test
+  (test)
+)
