fbd0ee44576ca86bc98895f31bd8bd1896838dcc
Chandra Yang <otama.ariq@gmail.com>
2017-07-13 02:35:20 +0700

    add digest-file and note that the implementation is buggy for long text

--- md5.rkt
+++ md5.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 ; MD5 implementation
+;;It is buggy when generate md5 for long text
 ;;based on rfc1321, wikipedia article, and https://raw.githubusercontent.com/CastixGitHub/racket-md5/master/md5.rkt
 ;;this is for owner learning only
 
@@ -73,6 +74,12 @@
            (~a #:width 2 #:pad-string "0" #:align 'right
                (number->string b 16)))))
 
+(define (digest-string str) (digest (string->bytes/utf-8 str)))
+
+(require 2htdp/batch-io)
+
+(define (digest-file path) (digest-string (read-file path)))
+  
 ;helper function
 ;;used to split the message into 64 bytes chunks
 ;;and to split each chunk into sixteen 4 bytes word
@@ -106,7 +113,8 @@
   (require test-engine/racket-tests)
   (check-expect (digest #"a") "0cc175b9c0f1b6a831c399e269772661")
   (check-expect (digest #"Runs all of the tests specified by check forms in the current module and reports the") "21c0b2cbeee1c110d97c32925a469eeb")
-  (check-expect (digest-string "a" "0cc175b9c0f1b6a831c399e269772661")
+  (check-expect (digest-string "a") "0cc175b9c0f1b6a831c399e269772661")
+  (check-expect (digest-file "/Users/chandra/Downloads/sicp.pdf") "3bed4a05ae7fc66cd90c2e292c70d2b4")
   ;;run test
   (test)
 )
