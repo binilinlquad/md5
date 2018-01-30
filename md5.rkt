bbb2ab28473bebba10295b31250e6c063e53c9d5
Chandra Yang <otama.ariq@gmail.com>
2018-01-31 01:32:21 +0700

    Reorganize and simplify some function
    - Simplify create input port for string, bytes, and file
    - Move helper functions to below file

--- md5.rkt
+++ md5.rkt
@@ -35,8 +35,7 @@
 
 ;I(X,Y,Z) = Y xor (X v not(Z))
 (define (I x y z)
-  (bitwise-xor y
-               (bitwise-ior x (bitwise-not z))))
+  (bitwise-xor y (bitwise-ior x (bitwise-not z))))
 
 ;; generator produce stream
 ;; in-generator is used to generator as sequence
@@ -46,41 +45,8 @@
 ;; 56 bytes = 448 bits
 (define block-size 64)
 
-;; Prepare String as input port
-(define (prepare-msg message)
-  (define msg-as-bytes
-    (cond [(string? message) (string->bytes/utf-8 message)]
-                             [(bytes? message) message]))
-  (define msg-bytes-length
-    (bytes-length msg-as-bytes))
-  (create-prepared-port
-   (open-input-bytes msg-as-bytes) msg-bytes-length))
-
-;; Prepare File as input port
-(define (prepare-file filename)
-  (create-prepared-port (open-input-file filename) (file-size filename)))
-
-;; Actual prepare input-port
-(define (create-prepared-port msg-input-port msg-bytes-length)
-  (define msg-bits-length (* msg-bytes-length 8))
-  (define pad-zero (make-bytes (- 56 (modulo (add1 msg-bytes-length) 64)) 0))
-  (let ([msg msg-input-port]
-        [bit1 (open-input-bytes (bytes #b10000000))]
-        [bit0 (open-input-bytes pad-zero)]
-        [length (open-input-bytes (integer->integer-bytes msg-bits-length 8 #f #f))])
-    (define port (input-port-append #t msg bit1 bit0 length))
-    ; body
-    (generate-block-sequence port block-size)))
-
-;; Generate prepared message as sequence
-(define (generate-block-sequence port block-size)
-      (in-generator (let loop ([blocks (read-bytes block-size port)])
-         (when (not (eof-object? blocks))
-           (yield blocks)
-           (loop (read-bytes block-size port))))))
-
 ;; Main Function
-(define (digest preprocessor msg)
+(define (digest msg-port)
   ;; convert bytes into integers
   (define (block->list/integer block)
     (for/list ([x (bytes-split block 4)])
@@ -88,7 +54,7 @@
   ;; Main Algorithm
   (define-values (a0 b0 c0 d0)
     (for/fold ([a0 #x67452301] [b0 #xefcdab89] [c0 #x98badcfe] [d0 #x10325476])
-              ([chunk-bytes (preprocessor msg)])
+              ([chunk-bytes msg-port])
       (define chunk (block->list/integer chunk-bytes))
       (define-values (A B C D)
         (for/fold ([A a0] [B b0] [C c0] [D d0])
@@ -103,28 +69,31 @@
           (define bTemp (word+ B (leftrotate (+ A F1 (list-ref K i) (list-ref chunk g)) (list-ref s i)))) ; M only be used here
           (values dTemp bTemp B C)))
       (values (word+ a0 A) (word+ b0 B) (word+ c0 C) (word+ d0 D))))
+  ; concat result as string
   (appendAll a0 b0 c0 d0))
 
-(define (appendAll a b c d)
-  (define all-bytes
-    (bytes-append
-     (integer->integer-bytes a 4 #f #f)
-     (integer->integer-bytes b 4 #f #f)
-     (integer->integer-bytes c 4 #f #f)
-     (integer->integer-bytes d 4 #f #f)))
-  (apply string-append
-         (for/list ([b all-bytes])
-           (~a #:width 2 #:pad-string "0" #:align 'right
-               (number->string b 16)))))
+(define (convert-to-bytes a b c d)
+  ; Convert passed integer of a, b, c, d into one bytes
+  (apply bytes-append
+         (map (lambda (number)
+                (integer->integer-bytes number 4 #f #f))
+              (list a b c d))))
 
-;; Helper
-;; used to split the message into 64 bytes chunks
-;; and to split each chunk into sixteen 4 bytes word
+(define (appendAll a b c d)
+  ; append all passed integers to a string
+  (let ([all-bytes (convert-to-bytes a b c d)])
+    (apply string-append
+           (for/list ([b all-bytes])
+             (~a #:width 2 #:pad-string "0" #:align 'right
+                 (number->string b 16))))))
+
+
 (define (bytes-split bytes size)
+  ; split message into 64 bytes chunks. Each chunk then splitted again into 16 of 4 bytes word
   (define len (bytes-length bytes))
   (unless (= 0 (modulo len size))
     ((raise-type-error
-     'split-bytes "byte string of length divisible by size" 0 bytes)))
+      'split-bytes "byte string of length divisible by size" 0 bytes)))
   (for/list ([i (in-range (/ len size))])
     (subbytes bytes (* size i) (* size (add1 i)))))
 
@@ -138,12 +107,48 @@
 (define (word+ w0 w1)
   (word-limit (+ w0 w1)))
 
+(define (message->bytes message)
+  ; convert message, i.e string or bytes format to bytes
+  (cond [(string? message) (string->bytes/utf-8 message)]
+        [(bytes? message) message]))
+
+(define (pad-zero-amount length-in-bytes)
+  (- 56 (modulo (add1 length-in-bytes) 64)))
+  
+(define (prepare-message text file?)
+  ; Prepare provided text as input port. Text could be string, bytes, or filepath
+  (define-values (msg-input-port msg-bytes-length)
+    (if file?
+        (values (open-input-file text) (file-size text))
+        (let ([msg-bytes (message->bytes text)])
+          (values (open-input-bytes msg-bytes) (bytes-length msg-bytes)))))
+  (create-prepared-port msg-input-port msg-bytes-length))
+
+(define (create-prepared-port msg-input-port msg-bytes-length)
+  ; Actual creation input-port for processing
+  (let* ([msg-bits-length (* msg-bytes-length 8)]
+         [pad-zero (make-bytes (pad-zero-amount msg-bytes-length) 0)]
+         [bit1 (open-input-bytes (bytes #b10000000))]
+         [bit0 (open-input-bytes pad-zero)]
+         [length (open-input-bytes (integer->integer-bytes msg-bits-length 8 #f #f))]
+         [port (input-port-append #t msg-input-port bit1 bit0 length)])
+    ; body
+    (generate-block-sequence port block-size)))
+
+(define (generate-block-sequence port block-size)
+  ; Generate prepared message as sequence
+  (in-generator (let loop ([blocks (read-bytes block-size port)])
+                  (when (not (eof-object? blocks))
+                    (yield blocks)
+                    (loop (read-bytes block-size port))))))
+
+
 ;; Shortcut
-(define (digest-bytes bytes) (digest prepare-msg bytes))
-(define (digest-string str) (digest prepare-msg str))
+(define (digest-bytes bytes) (digest (prepare-message bytes #f)))
+(define (digest-string str) (digest (prepare-message str #f)))
 
 (require 2htdp/batch-io)
-(define (digest-file path) (digest prepare-file path))
+(define (digest-file path) (digest (prepare-message path #t)))
 
 ;; test
 (module+ test ;; add submodule test
@@ -160,4 +165,4 @@
 
   ;;run test
   (test)
-)
+  )
