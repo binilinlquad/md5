ca84c9bb2be67bd20bf3dc153bef6247d824e0e2
chandra <otama.ariq@gmail.com>
2020-04-15 00:08:31 +0700

    Fix cannot compile after upgrade to drRacket 7.3
    Cannot compile because of `check-within` has been required
    from other so will cause `(require test-engine/racket-tests)`
    failed.
    
    Get inspiration from here https://stackoverflow.com/a/55445918/3550923
    and Racket `require` reference to exclude `check-within`.

--- md5.rkt
+++ md5.rkt
@@ -202,7 +202,8 @@
   (check-equal? (digest-bytes #"Runs all of the tests specified by check forms in the current module and reports the") "21c0b2cbeee1c110d97c32925a469eeb")
   (check-not-equal? (digest-bytes #"Runs all of the tests specified by check forms in the current module and reports the ") "21c0b2cbeee1c110d97c32925a469eeb")
 
-  (require test-engine/racket-tests)
+  (require (except-in test-engine/racket-tests
+                      check-within))
   (check-expect (digest-bytes #"a") "0cc175b9c0f1b6a831c399e269772661")
   (check-expect (digest-bytes #"Runs all of the tests specified by check forms in the current module and reports the") "21c0b2cbeee1c110d97c32925a469eeb")
   (check-expect (digest-string "a") "0cc175b9c0f1b6a831c399e269772661")
